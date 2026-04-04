06f7bc62631d910789710afb65e4418d89a02e28
iunlockersolutions <iunlockersolutions.ieg@gmail.com>
2026-04-04 10:07:38 +0530

    update

--- app/ops/models/[id]/page.tsx
+++
@@ -1,2 +0,0 @@
-export { metadata } from "@/app/ops/product-model-groups/[id]/page"
-export { default } from "@/app/ops/product-model-groups/[id]/page"
--- app/ops/models/new/page.tsx
+++
@@ -1,2 +0,0 @@
-﻿export { metadata } from "@/app/ops/product-model-groups/new/page"
-export { default } from "@/app/ops/product-model-groups/new/page"
